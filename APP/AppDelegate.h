680d6751b6e323a1cda3215905670431bf417fa5
August Sun <augustsun@Augusts-Mac.local>
2014-07-30 19:27:09 -0400

    Initial Commit

---
+++ APP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  APP
+//
+//  Created by August Sun on 7/30/14.
+//  Copyright (c) 2014 KRAIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ APPTests/APPTests.m
@@ -0,0 +1,34 @@
+//
+//  APPTests.m
+//  APPTests
+//
+//  Created by August Sun on 7/30/14.
+//  Copyright (c) 2014 KRAIL. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface APPTests : XCTestCase
+
+@end
+
+@implementation APPTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
